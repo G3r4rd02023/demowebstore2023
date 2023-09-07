1d9c212d199c5f181ca12871e2447bf2b632ce9e
Gerardo Lanza <glanza007@gmail.com>
2023-09-06 19:24:58 -0600

    crud design complete

---
+++ Webstore2023/Webstore2023/Controllers/CountriesController.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Webstore2023.Data;
+using Webstore2023.Data.Entities;
+
+namespace Webstore2023.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+       
+        public async Task<IActionResult> Index()
+        {
+            var country = await _context.Countries
+             .ToListAsync();
+            return View(country);
+        }
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(country);
+                    await _context.SaveChangesAsync();
+                    TempData["SuccessMessage"] = "País creado con éxito.";
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            TempData["ErrorMessage"] = "Hubo un error al crear el país.";
+            return View(country);
+        }
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return View(country);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,  Country country)
+        {
+            if (id != country.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(country);
+                    await _context.SaveChangesAsync();
+                    TempData["SuccessMessage"] = "País actualizado con éxito.";
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(country);
+        }
+
+      
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Countries.Remove(country);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Registro borrado con éxito.";
+            }
+            catch
+            {
+                ModelState.AddModelError(string.Empty, "No puede eliminar, el registro esta relacionado.");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+               
+        
+    }
+}
---
+++ Webstore2023/Webstore2023/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webstore2023.Data.Entities;
+
+namespace Webstore2023.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+
+}
